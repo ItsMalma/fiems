4439b9f578b4835d3401c6401b32d8743bf0cae0
ItsMalma <itsmalma28@gmail.com>
2024-01-05 23:36:47 +0700

    feat[operational]: add job order report

---
+++ src/app/(app)/operational/jobOrder/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { JobOrderDTO, getAllJobOrder } from "@/actions/jobOrder";
+import ReportLayout from "@/components/layouts/ReportLayout";
+import { useAction } from "@/lib/hooks";
+import { containerSizes, containerTypes, typeOrders } from "@/lib/utils/consts";
+import { actionColumn, dateColumn, textColumn } from "@/lib/utils/tableColumns";
+import { useMenu } from "@/stores/useMenu";
+import { ColumnsType } from "antd/es/table";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function JobOrder() {
+  const { setKey } = useMenu();
+  React.useEffect(() => {
+    setKey("operational.jobOrder");
+  }, [setKey]);
+
+  const [jobOrders, refresh] = useAction(getAllJobOrder);
+
+  const router = useRouter();
+  const columns: ColumnsType<JobOrderDTO> = [
+    dateColumn("createDate"),
+    textColumn("number"),
+    textColumn("inquiryDetail.inquiryNumber", "Inquiry Number"),
+    textColumn("inquiryDetail.inquiry.salesName", "Sales Name"),
+    textColumn("inquiryDetail.shippingName", "Shipping Name"),
+    textColumn("inquiryDetail.vesselName", "Vessel Name"),
+    textColumn("inquiryDetail.voyage", "Voyage"),
+    dateColumn("inquiryDetail.etd", "ETD"),
+    dateColumn("inquiryDetail.eta", "ETA"),
+    dateColumn("inquiryDetail.loadDate", "Load Date"),
+    textColumn("inquiryDetail.inquiry.shipperName", "Shipper Name"),
+    textColumn("inquiryDetail.deliveryToName", "Delivery To"),
+    textColumn("inquiryDetail.routeDescription", "Route Description"),
+    textColumn("inquiryDetail.containerSize", "Container Size", containerSizes),
+    textColumn("inquiryDetail.containerType", "Container Type", containerTypes),
+    textColumn("inquiryDetail.typeOrder", "Type Order", typeOrders),
+    textColumn("roNumber", "RO Number"),
+    textColumn("consigneeName"),
+    dateColumn("stuffingDate"),
+    textColumn("trackingRouteDescription"),
+    textColumn("trackingVendorName"),
+    textColumn("truckNumber"),
+    textColumn("driverName"),
+    textColumn("containerNumber1", "Container Number 1"),
+    textColumn("sealNumber1", "Seal Number 1"),
+    textColumn("containerNumber2", "Container Number 2"),
+    textColumn("sealNumber2", "Seal Number 2"),
+    actionColumn({
+      onView: (record) => {
+        router.replace(
+          `/operational/jobOrder/save?number=${record["number"]}&view=1`
+        );
+      },
+    }),
+  ];
+
+  return (
+    <ReportLayout
+      name="Job Order"
+      saveUrl="/operational/jobOrder/save"
+      columns={columns}
+      data={jobOrders}
+      rowKey="number"
+    />
+  );
+}
