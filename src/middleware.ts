ab29c3907ba955bd29eb680e67faed0ff31bec4f
ItsMalma <itsmalma28@gmail.com>
2024-01-08 23:54:37 +0700

    fix[auth]: user can access menu via url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { UserDTO } from "./actions/auth";
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token");
@@ -16,6 +17,18 @@ export async function middleware(req: NextRequest) {
     })
   ).json();
   if (!res.data) return NextResponse.rewrite(new URL("/login", req.nextUrl));
+
+  const user: UserDTO = res.data;
+
+  const baseMenu = req.nextUrl.pathname.split("/")[1];
+  if (
+    baseMenu &&
+    !user.accesses.find(
+      (access) => access.name === baseMenu && access.actions.includes("Read")
+    )
+  ) {
+    return NextResponse.rewrite(new URL("/login", req.nextUrl));
+  }
 }
 
 export const config = {
